800941533f3194ff194ace0d64a483b9a83e83f8
Shaun Gordon <shaundev.gordon@gmail.com>
2016-02-21 16:59:04 +0000

    Add trashed to table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221141433) do
+ActiveRecord::Schema.define(version: 20160221165726) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160221141433) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.boolean  "public",                    null: false
+    t.boolean  "trashed",                   null: false
   end
 
   create_table "projects_uploads", id: false, force: :cascade do |t|
